4778930d5fafa738e65a1e9dddfeaad0d8ca0914
Tammanna TN <tammanna8886@gmail.com>
2025-09-25 14:54:42 +0530

    Task_3.sql

--- Task_3.sql
+++ Task_3.sql
@@ -92,7 +92,7 @@ SELECT product_name, stock
 FROM Products ORDER BY stock DESC;
 
 
--- Here are example SQL operations for the "" table including 
+-- Here are example SQL operations for the "order" table including 
 -- use of SELECT (all and specific columns), WHERE conditions (with AND, OR, LIKE, BETWEEN), and sorting with ORDER BY. --
 
 -- Select all columns -- 
